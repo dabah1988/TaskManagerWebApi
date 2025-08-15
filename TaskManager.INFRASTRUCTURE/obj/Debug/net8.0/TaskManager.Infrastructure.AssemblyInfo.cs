f69b86e17082ed3ba08afb920df878d3981988fc
dabson1988 <dabson2012@gmail.com>
2025-08-15 21:02:02 +0200

    Add function SearchBy

--- TaskManager.INFRASTRUCTURE/obj/Debug/net8.0/TaskManager.Infrastructure.AssemblyInfo.cs
+++ TaskManager.INFRASTRUCTURE/obj/Debug/net8.0/TaskManager.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46d7d7447de7a19268db2229c05313fee89dd687")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+054c4cdeed9775adc190709ea1e96eca34b45989")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
